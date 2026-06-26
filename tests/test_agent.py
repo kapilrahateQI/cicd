e121b84565c4fc2d40eef2e1c3f93544b086f1e4
kapilrahateQI <kapil.rahate@quantumintegrators.com>
2026-06-26 23:56:39 +0530

    add path to test_agent

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,4 +1,10 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 from agent import root_agent
+
 
 def test_agent_exists():
     assert root_agent is not None
